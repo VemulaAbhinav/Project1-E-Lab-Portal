aecbd206278e68bd78b3f98b6772c1cd286866c3
madhumad007 <55804416+madhumad007@users.noreply.github.com>
2020-05-25 20:16:01 +0530

    Added new feature 0.1

---
+++ page1.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+ </head>
+ <body>
+   <h1>Hello World this is page 1<h1>
+  <form action="page2.php" method="POST" name="form1" onsubmit="document.form1.target='page2'; return true;">
+  <input type="submit" name="" value="simulate" >
+  </form>
+ </body>
+</html>
