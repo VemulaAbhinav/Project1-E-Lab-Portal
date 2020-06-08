21a39fa44318c106cfe71fa0bbe1de93af5d7926
Abhinav Vemula <abhivemula1372@gmail.com>
2020-06-08 18:22:48 +0530

    changes again

--- page1.php
+++ page1.php
@@ -9,21 +9,24 @@ require_once("header1.php");
  <head>
  </head>
  <body>
-   <h1>Hello World this is page 1<h1>
+   <h1 style= "color:white;"> Week Programs <h1>
   <form action="page2.php" method="POST" name="form1" onsubmit="document.form1.target='side2'; return true;">
   <input type="submit" name="" value="week1 program 1"  >
 </form>
+<br>
 
  <form action="page3.php" method="POST" name="form2" onsubmit="document.form2.target='side2'; return true;">
  <input type="submit" name="" value="week 1 program 2"  >
 </form>
+<br>
 
 <form action="page4.php" method="POST" name="form3" onsubmit="document.form3.target='side2'; return true;">
 <input type="submit" name="" value="week 1 program 3"  >
 </form>
+<br>
 
 
-<h2> Back to Home page <h2>
+<h2 style= "color:white;"> Back to Home page <h2>
 
    <form action='home.php' method="POST" name="Back to Home" target="_parent" >
     <input type="submit" name="" value="Back to Portal" >
